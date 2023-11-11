23995458acb6500685f99b6c3911ba6bad1c783a
janvierjr <bernie@myheartsleeve.com>
2023-11-11 11:51:02 -0600

    create components from product-display and product-details

---
+++ components/ProductDisplay.js
@@ -0,0 +1,115 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template:
+    /*html*/
+    `<div class="product-display">
+        <div class="product-container">
+          <div class="product-image">
+            <img
+              :src="image"
+              :class="{ 'out-of-stock-img': !inStock }"
+            />
+          </div>
+          <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else="inStock">Out of Stock</p>
+
+            <p>Shipping: {{ shipping }}</p>
+
+            <p>{{ sale }}</p>
+
+            <product-details :details="details"></product-details>
+
+            <div
+            v-for="(variant, index) in variants"
+            :key="variant.id"
+            @mouseover="updateVariant(index)"
+            class="color-circle"
+            :style="{ backgroundColor: variant.color }"
+            >
+          </div>
+            <button
+              class="button"
+              :class="{ disabledButton: !inStock}"
+              @click="addToCart"
+              :disabled="!inStock"
+              >
+              Add to Cart
+            </button>
+            <button
+              class="button"
+              :class="{ disabledButton: !inStock }"
+              style="width: fit-content;"
+              @click="removeFromCart"
+              :disabled="!inStock"
+              >Remove from Cart</button>
+        </div>
+      </div>
+    </div>`,
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      selectedVariant: 0,
+      variants: [
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+          sale: true,
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+          sale: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.variants[this.selectedVariant].sale) {
+        return this.brand + ' ' + this.product + ' are on sale.';
+      }
+      return 'Special offer';
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free';
+      }
+      return '2.99';
+    },
+  },
+});
