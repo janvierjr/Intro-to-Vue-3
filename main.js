e0c51c302766bc239a77eb402f3925eb80aaf690
janvierjr <bernie@myheartsleeve.com>
2023-11-12 14:46:26 -0600

    update cart with item id when Add to Cart or Remove from Cart is clicked

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      premium: true
+      cart: [],
+      premium: true,
     };
   },
-  methods: {}
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+      console.log(this.cart)
+    },
+    removeFromCart(id) {
+      if (this.cart.length > 0) {
+        this.cart.splice(this.cart.indexOf(id), 1)
+      }
+      console.log(this.cart);
+    },
+  }
 });
